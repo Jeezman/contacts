c48e62491efe826f45ce2419980da0ec2546002e
Olutobi Adeyemi <tobiadeyemi91@gmail.com>
2017-07-04 18:59:34 +0100

    comment non useful code

--- src/App.js
+++ src/App.js
@@ -52,77 +52,163 @@ class App extends Component {
   }
 }
 
-// var acc = document.getElementsByClassName("accordion");
-// var i;
 
-// for (i = 0; i < acc.length; i++) {
-//   acc[i].onclick = function() {
-//     this.classList.toggle("active");
-//     var panel = this.nextElementSibling;
-//     if (panel.style.maxHeight){
-//       panel.style.maxHeight = null;
-//     } else {
-//       panel.style.maxHeight = panel.scrollHeight + "px";
-//     } 
+// class Accordion extends Component {
+
+//   state = {
+//     active: false,
+//     title: '',
+//     sections: [
+//       { name: 'Applicants', 
+//         section:[
+//           {name: 'All'},
+//           {name: 'Pending'}
+//       ]},
+//       {name: 'Settings', section: [
+//         {name: 'Settings'}
+//       ]},
+//       {name: 'API', section: [
+//         {name: 'API'}
+//       ]},
+//       {name: 'Help', section: [
+//         {name: 'Help'}
+//       ]},
+//       {name: 'Shared Results', section: [
+//         {name: 'Shared Results'}
+//       ]}
+//   ]
+//   }
+
+//   handlePanelShow = () => {
+//      console.log('hello world');
+//      const currentState = this.state.active;
+//      this.setState({
+//        active: !currentState
+//      })
+//      console.log(this.state.active)
+//   }
+  
+//   render() {
+//     return (
+//       <div className="accordion__wrap">
+        
+//         <AccordionHeader 
+//           title={this.props.title}
+//           toggle={this.state.active}
+//           panelShow={this.handlePanelShow}
+//         />
+
+//         <AccordionBody togglePanel={this.state.active} >
+//           <p style={{background: 'skyblue', margin: 0}}>Tosin</p>
+//           <p style={{background: 'coralwhite', margin: 0}}>Felix</p>
+//           <p style={{background: 'skyblue', margin: 0}}>Lorem</p>
+//           <p style={{background: 'coralwhite', margin: 0}}>Lorem ipsum </p>
+//           <p style={{background: 'skyblue', margin: 0}}>Faith</p>
+//           <p style={{background: 'coralwhite', margin: 0}}>Dropdown</p>
+//           <p style={{background: 'green', margin: 0}}>Famous</p>
+//           <p style={{background: 'lime', margin: 0}}>Gbenga</p>
+//           <p style={{background: 'green', margin: 0}}>Suru</p>
+//           <p style={{background: 'lime', margin: 0}}>Mosunmola</p>
+//         </AccordionBody>
+
+//         {
+//           this.props.section.map(menu => {
+            
+//           })
+//         }
+//       </div>
+//     )
+//   }
+// }
+
+// class AccordionHeader extends Component {
+     
+//   render () {
+//     return (
+//       <button 
+//         className={!this.props.toggle ? 'accordion' : 'accordion-active' }
+//         onClick={this.props.panelShow}>
+//         {this.props.title}
+//       </button>
+//     )
+//   }
+// }
+
+// class AccordionBody extends Component {
+//   render () {
+//     return (
+//       <div ref='divScroll' className={!this.props.togglePanel ? 'panel' : 'panel-toggle'} >
+//         {this.props.children}
+//       </div>
+//     )
+//   }
+// }
+
+// class Boss extends Component {
+
+//   state = {
+//     active: false,
+//     title: '',
+//     sections: [
+//       { name: 'Applicants', 
+//         section:[
+//           {name: 'All'},
+//           {name: 'Pending'}
+//       ]},
+//       {name: 'Settings', section: [
+//         {name: 'Settings'}
+//       ]},
+//       {name: 'API', section: [
+//         {name: 'API'}
+//       ]},
+//       {name: 'Help', section: [
+//         {name: 'Help'}
+//       ]},
+//       {name: 'Shared Results', section: [
+//         {name: 'Shared Results'}
+//       ]}
+//   ]
+//   }
+//   render () {
+//     return (
+//       <div>
+//         {this.state.sections.map((value) =>
+//           <Accordion key={value.name} title={value.name} section={this.state.sections} />
+//         )}
+//       </div>
+//     )
 //   }
 // }
 
 
-class Accordion extends Component {
-  
-  render() {
-    return (
-      <div className="acc">
-        <AccordionHeader 
-          title='Section 1'
-        />
-        <AccordionBody>
-          <p>Lorem ipsum meta sit amet</p>
-          <p>Lorem ipsum meta sit amet</p>
-        </AccordionBody>
-      </div>
-    )
-  }
-}
-
-class AccordionHeader extends Component {
-
-  state = {
-    active: false
-  }
-
-  handlePanelShow = () => {
-     console.log('hello world');
-     const currentState = this.state.active;
-     this.setState({
-       active: !currentState
-     })
-     console.log(this.state.active)
-
-  }
-     
-
-  render () {
-    return (
-      <button 
-        className={`accordion  this.state.active ? 'active': null`}
-        onClick={this.handlePanelShow}>
-        {this.props.title}
-      </button>
-    )
-  }
-}
-
-class AccordionBody extends Component {
-  render () {
-    return (
-      <div className="panel" >
-        {this.props.children}
-      </div>
-    )
-  }
-}
-
-
-
 export default App;
+
+// var sections = [
+//     { name: 'Applicants', 
+//       section:[
+//         {name: 'All'},
+//         {name: 'Pending'}
+//     ]},
+//     {name: 'Settings', section: [
+//       {name: 'Settings'}
+//     ]},
+//     {name: 'API', section: [
+//       {name: 'API'}
+//     ]},
+//     {name: 'Help', section: [
+//       {name: 'Help'}
+//     ]},
+//     {name: 'Shared Results', section: [
+//       {name: 'Shared Results'}
+//     ]}
+// ];
+
+// var sections_applicants = [
+//   {name: 'All'},
+//   {name: 'Pending'},
+//   {name: 'Awaiting Action'},
+//   {name: 'In Progress'},
+//   {name: 'Complete'},
+//   {name: 'Cancelled'},
+//   {name: 'Withdrawn'}
+// ]
